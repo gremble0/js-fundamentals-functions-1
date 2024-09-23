243ce7b049f67b30700cdff090398aaf3b5d6066
gremble0 <hermanstornes@gmail.com>
2024-09-23 11:40:48 +0200

    Complete extension

--- src/extensions/creating-functions-multiple-args.js
+++ src/extensions/creating-functions-multiple-args.js
@@ -9,6 +9,13 @@
 // -1, 1        | [-1, 0, 1]
 //
 // TODO: write code below
+function between(lower, upper) {
+  const out = []
+
+  for (let i = lower; i <= upper; ++i) out.push(i)
+
+  return out
+}
 
 // 2. define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
@@ -21,6 +28,9 @@
 // error, 10    | ERROR!!!!!!!!!!
 //
 // TODO: write code below
+function exclamate(s, numExclams) {
+  return s.toUpperCase() + '!'.repeat(numExclams)
+}
 
 // 3. define a function that takes two arguments: a string and a number.
 // The function should return the new time in 24hr time after adding the additional time in minutes.
@@ -33,10 +43,25 @@
 // '12:50', 120 | '14:50'
 // '23:50', 30  | '00:20'
 // TODO: write code below
+function addMinutesToTime(time, minutes) {
+  let [hours, mins] = time.split(':').map(Number)
+
+  let totalMinutes = hours * 60 + mins + minutes
+
+  let newHours = Math.floor(totalMinutes / 60) % 24
+  let newMins = totalMinutes % 60
+
+  if (newHours === 0)
+    return `${String(newHours).padStart(2, '0')}:${String(newMins).padStart(
+      2,
+      '0'
+    )}`
+  else return `${String(newHours)}:${String(newMins).padStart(2, '0')}`
+}
 
 // TODO: change the exported value to be the name of the function you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
-  c: undefined // etc
+  a: between, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: exclamate, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  c: addMinutesToTime // etc
 }
